4c89873c25527052ced819f079c8c1ed1e2bbad5
sungchan8000 <sungchan8000@kakao.com>
2023-03-01 20:04:23 +0900

    토비의 spring boot 1, 프로젝트 생성

---
+++ toby_spring_boot/src/main/java/com/study/toby_spring_boot/TobySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.study.toby_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringBootApplication.class, args);
+    }
+
+}
