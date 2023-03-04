3d0cc40cb77d50d456c8bdf1fe72c139e64f76e0
sungchan8000 <sungchan8000@kakao.com>
2023-03-04 14:21:24 +0900

    ch 05

---
+++ do_it_java_programming/src/ch05/IntVariableTest.java
@@ -0,0 +1,12 @@
+package ch05;
+
+public class IntVariableTest {
+    public static void main(String[] args) {
+        byte bs = -128;
+        System.out.println(bs);
+
+        //long 자료형
+        long iVal = 12345678900L;
+        System.out.println(iVal);
+    }
+}
