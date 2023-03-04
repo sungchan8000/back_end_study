39134cd491f96aaa7a0f92e58ab516a454c2786e
sungchan8000 <sungchan8000@kakao.com>
2023-03-04 12:33:17 +0900

    ch 02 2진수와 10진수, 16진수

---
+++ do_it_java_programming/src/ch02/BinaryTest.java
@@ -0,0 +1,20 @@
+package ch02;
+
+public class BinaryTest {
+    public static void main(String[] args) {
+        //10 진수
+        int num = 10;
+        // 2진수
+        int bNum = 0b1010;
+        // 8진수
+        int oNum = 012;
+        // 16진수
+        int xNum = 0xA;
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(xNum);
+
+    }
+}
